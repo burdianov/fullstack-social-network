b206f3f2abdbaa4f28a275afea2d23f62c98c668
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-09-28 23:37:56 +0400

    mongo db

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const morgan = require("morgan");
+const dotenv = require('dotenv');
+dotenv.config();
+
+// db
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("DB Connected");
+});
+
+mongoose.connection.on('error', err => {
+    console.log(`DB connection error: ${err.message}`);
+});
 
 // routes
 const postRoutes = require("./routes/post");
@@ -10,7 +25,7 @@ app.use(morgan("dev"));
 
 app.use("/", postRoutes);
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 app.listen(8080, () => {
     console.log(`Server running on port ${port} ...`);
 });
