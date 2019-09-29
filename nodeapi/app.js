0c9086b6235da8d672f746129452b6a44a925970
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-09-29 22:29:14 +0400

    create a post

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const morgan = require("morgan");
 const dotenv = require('dotenv');
 dotenv.config();
@@ -22,6 +23,7 @@ const postRoutes = require("./routes/post");
 
 // middleware
 app.use(morgan("dev"));
+app.use(bodyParser.json());
 
 app.use("/", postRoutes);
 
