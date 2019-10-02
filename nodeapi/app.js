975f0916059f6842c8f1a04d62261a4c53f05f50
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-10-02 22:37:46 +0400

    user signin and jwt

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const morgan = require("morgan");
 const dotenv = require('dotenv');
 dotenv.config();
@@ -25,6 +26,7 @@ const authRoutes = require("./routes/auth");
 // middleware
 app.use(morgan("dev"));
 app.use(bodyParser.json());
+app.use(cookieParser());
 app.use("/", postRoutes);
 app.use("/", authRoutes);
 
--- nodeapi/controllers/auth.js
+++ nodeapi/controllers/auth.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 const User = require('../models/user');
 const {validationResult} = require('express-validator');
 
@@ -19,4 +21,32 @@ exports.signUp = async (req, res) => {
     const user = await new User(req.body);
     await user.save();
     res.status(200).json({message: "Signup success! Please login."})
+};
+
+exports.signIn = (req, res) => {
+    const {email, password} = req.body;
+    User.findOne({email}, (err, user) => {
+        if (err || !user) {
+            return res.status(401).json({
+                error: "User with this email does not exist. Please signup"
+            })
+        }
+        if (!user.authenticate(password)) {
+            return res.status(401).json({
+                error: "Wrong email or password"
+            })
+        }
+        // generate a token with user id and secret
+        const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET);
+
+        // provide the token to the frontend in two ways - user will decide
+        // which one to use
+        // 1: persist the token as 'tkn' in cookie with expiry date
+        res.cookie("tkn", token, {expire: new Date() + 360000});
+
+        // 2: return response with user and token to frontend client
+        const {_id, name, email} = user;
+        return res.json({token, user: {_id, email, name}})
+    });
+
 };
