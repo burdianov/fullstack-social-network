878d5133333a3f9ecb510cef0491727f6ad3fcab
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-10-05 21:51:50 +0400

    create post with image upload and user

--- nodeapi/controllers/post.js
+++ nodeapi/controllers/post.js
@@ -1,16 +1,18 @@
 const Post = require("../models/post");
 const {validationResult} = require("express-validator");
+const formidable = require("formidable");
+const fs = require("fs");
 
 exports.getPosts = async (req, res) => {
     try {
         const posts = await Post.find().select("_id title body");
         await res.json({posts});
-    } catch(err) {
+    } catch (err) {
         console.error(err);
     }
 };
 
-exports.createPost = (req, res) => {
+exports.createPost = (req, res, next) => {
     // check for errors
     const {errors} = validationResult(req);
 
@@ -21,10 +23,28 @@ exports.createPost = (req, res) => {
             error: firstError
         });
     }
-    const post = new Post(req.body);
-    post.save().then(result => {
-        res.json({
-            post: result
-        })
+
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            return res.status(400).json({
+                error: "Image could not be uploaded."
+            })
+        }
+        let post = new Post(fields);
+        post.postedBy = req.profile;
+        if (files.photo) {
+            post.photo.data = fs.readFileSync(files.photo.path);
+            post.photo.contentType = files.photo.type;
+        }
+        post.save((err, result) => {
+            if (err) {
+                return res.status(400).json({
+                    error: err
+                })
+            }
+            res.json(result);
+        });
     });
 };
