d7d138061cde49b83fb7b34bb8c7eec303e29b41
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-10-12 00:35:58 +0400

    user signout

--- react-front/src/components/core/Menu.jsx
+++ react-front/src/components/core/Menu.jsx
@@ -9,18 +9,45 @@ const isActive = (history, path) => {
     }
 };
 
+export const signout = (next) => {
+    if (typeof window !== "undefined") {
+        localStorage.removeItem("jwt")
+    }
+    next();
+    return fetch("http://localhost:8080/signout", {
+        method: "GET"
+    })
+        .then(response => {
+            console.log('signout', response);
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
 const Menu = ({history}) => {
     return (
         <div>
             <ul className="nav nav-tabs bg-primary">
                 <li className="nav-item">
-                    <Link className="nav-link" style={isActive(history, "/")} to="/">Home</Link>
+                    <Link className="nav-link" style={isActive(history, "/")}
+                          to="/">Home</Link>
                 </li>
                 <li className="nav-item">
-                    <Link className="nav-link" style={isActive(history, "/signin")} to="/signin">Sign In</Link>
+                    <Link className="nav-link"
+                          style={isActive(history, "/signin")} to="/signin">Sign
+                        In</Link>
                 </li>
                 <li className="nav-item">
-                    <Link className="nav-link" style={isActive(history, "/signup")} to="/signup">Sign Up</Link>
+                    <Link className="nav-link"
+                          style={isActive(history, "/signup")} to="/signup">Sign
+                        Up</Link>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link"
+                       style={isActive(history, "/signout"), {cursor: "pointer", color:"#fff"}}
+                       onClick={() => signout(() => {
+                           history.push('/');
+                       })}>Sign Out</a>
                 </li>
             </ul>
         </div>
