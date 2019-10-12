a7a6f9ed565763e1e9e77d4f80a6ca94c8be7a0a
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-10-12 22:40:26 +0400

    active link user profile

--- react-front/src/components/core/Menu.jsx
+++ react-front/src/components/core/Menu.jsx
@@ -3,57 +3,61 @@ import {Link, withRouter} from "react-router-dom";
 import {isAuthenticated, signout} from "../../auth";
 
 const isActive = (history, path) => {
-    if (history.location.pathname === path) {
-        return {color: "#ff9900"};
-    } else {
-        return {color: "#ffffff"}
-    }
+  if (history.location.pathname === path) {
+    return {color: "#ff9900"};
+  } else {
+    return {color: "#ffffff"}
+  }
 };
 
 const Menu = ({history}) => {
-    return (
-        <div>
-            <ul className="nav nav-tabs bg-primary">
-                <li className="nav-item">
-                    <Link className="nav-link" style={isActive(history, "/")}
-                          to="/">Home</Link>
-                </li>
-                {!isAuthenticated() && (
-                    <>
-                        <li className="nav-item">
-                            <Link className="nav-link"
-                                  style={isActive(history, "/signin")}
-                                  to="/signin">Sign
-                                In</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link"
-                                  style={isActive(history, "/signup")}
-                                  to="/signup">Sign
-                                Up</Link>
-                        </li>
-                    </>
-                )}
-                {isAuthenticated() && (
-                    <>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/"
-                                  style={{cursor: "pointer", color: "#fff"}}
-                                  onClick={() => signout(() => {
-                                      history.push('/');
-                                  })}>
-                                Sign Out
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to={`/user/${isAuthenticated().user._id}`}>
-                                {`${isAuthenticated().user.name}'s profile`}
-                            </Link>
-                        </li>
-                    </>)}
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <ul className="nav nav-tabs bg-primary">
+        <li className="nav-item">
+          <Link className="nav-link" style={isActive(history, "/")}
+                to="/">Home</Link>
+        </li>
+        {!isAuthenticated() && (
+          <>
+            <li className="nav-item">
+              <Link className="nav-link"
+                    style={isActive(history, "/signin")}
+                    to="/signin">Sign
+                In</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link"
+                    style={isActive(history, "/signup")}
+                    to="/signup">Sign
+                Up</Link>
+            </li>
+          </>
+        )}
+        {isAuthenticated() && (
+          <>
+            <li className="nav-item">
+              <Link className="nav-link" to="/"
+                    style={{cursor: "pointer", color: "#fff"}}
+                    onClick={() => signout(() => {
+                      history.push('/');
+                    })}>
+                Sign Out
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link"
+                style={
+                  isActive(history, `/user/${isAuthenticated().user._id}`)}
+                to={`/user/${isAuthenticated().user._id}`}>
+                {`${isAuthenticated().user.name}'s profile`}
+              </Link>
+            </li>
+          </>)}
+      </ul>
+    </div>
+  );
 };
 
 export default withRouter(Menu);
