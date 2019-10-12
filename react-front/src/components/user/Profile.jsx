f4d8094fbc6eca2941101827cb337ad9c634fc88
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-10-12 20:46:30 +0400

    fetch user profile

--- react-front/src/components/user/Profile.jsx
+++ react-front/src/components/user/Profile.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {isAuthenticated} from "../../auth";
+import {Redirect} from "react-router-dom";
 
 const Profile = (props) => {
     const [user, setUser] = useState("");
@@ -8,16 +9,38 @@ const Profile = (props) => {
     const {name, email} = isAuthenticated().user;
 
     useEffect(() => {
-        console.log("user id from route params: ", props.match.params.userId)
+        const userId = props.match.params.userId;
+        fetch(`${process.env.REACT_APP_API_URL}/user/${userId}`, {
+            method: "GET",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${isAuthenticated().token}`
+            }
+        })
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                if (data.error) {
+                    setRedirectToSignin(true);
+                } else {
+                    setUser(data);
+                }
+            });
     }, []);
-
-    return (
-        <div className="container">
-            <h2 className="mt-5 mb-5">Profile</h2>
-            <p>Hello {name}</p>
-            <p>Email: {email}</p>
-        </div>
-    );
+    
+    if (redirectToSignin) {
+        return <Redirect to="/signin"/>
+    } else {
+        return (<div className="container">
+                <h2 className="mt-5 mb-5">Profile</h2>
+                <p>Hello {name}</p>
+                <p>Email: {email}</p>
+                <p>{`Joined ${new Date(user.createdAt).toDateString()}`}</p>
+            </div>
+        );
+    }
 };
 
 export default Profile;
