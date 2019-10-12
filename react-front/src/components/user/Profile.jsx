58320c36dd9775eec1ddd776041bf69d22a5461d
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-10-12 19:39:12 +0400

    showing user from localstorage

--- react-front/src/components/user/Profile.jsx
+++ react-front/src/components/user/Profile.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {isAuthenticated} from "../../auth";
 
-const Profile = () => {
+const Profile = (props) => {
+    const [user, setUser] = useState("");
+    const [redirectToSignin, setRedirectToSignin] = useState(false);
+
+    const {name, email} = isAuthenticated().user;
+
+    useEffect(() => {
+        console.log("user id from route params: ", props.match.params.userId)
+    }, []);
+
     return (
         <div className="container">
             <h2 className="mt-5 mb-5">Profile</h2>
+            <p>Hello {name}</p>
+            <p>Email: {email}</p>
         </div>
     );
 };
