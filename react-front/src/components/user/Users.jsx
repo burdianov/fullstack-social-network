dd16b7933ad833ae9dffc3f068c672159c34dfcc
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-10-13 22:51:02 +0400

    loop through the users

--- react-front/src/components/user/Users.jsx
+++ react-front/src/components/user/Users.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {getAllUsers} from "../../api/user";
 
 const Users = () => {
@@ -15,8 +15,17 @@ const Users = () => {
   });
 
   return (
-    <div  className="container">
+    <div className="container">
       <h2 className="mt-5 mb-5">Users</h2>
+      <div className="card">
+        {users.map((user, i)=> {
+          return (
+            <div key={i}>
+              <p>{user.name}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
