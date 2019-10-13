225bee4eafcbf55f031ec5fc05e2a1e5fb29ae81
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-10-13 22:35:08 +0400

    populate the state with the users

--- react-front/src/api/user.js
+++ react-front/src/api/user.js
@@ -1,16 +1,28 @@
 export const getUserProfile = (userId, token) => {
-    return fetch(`${process.env.REACT_APP_API_URL}/user/${userId}`, {
-        method: "GET",
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`
-        }
+  return fetch(`${process.env.REACT_APP_API_URL}/user/${userId}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`
+    }
+  })
+    .then(response => {
+      return response.json();
     })
-        .then(response => {
-            return response.json();
-        })
-        .catch(err => {
-            console.log(err);
-        });
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+export const getAllUsers = () => {
+  return fetch(`${process.env.REACT_APP_API_URL}/users`, {
+    method: "GET"
+  })
+    .then(response => {
+      return response.json();
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
--- react-front/src/components/user/Users.jsx
+++ react-front/src/components/user/Users.jsx
@@ -1,6 +1,19 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import {getAllUsers} from "../../api/user";
 
 const Users = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getAllUsers().then(data => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        setUsers(data);
+      }
+    });
+  });
+
   return (
     <div  className="container">
       <h2 className="mt-5 mb-5">Users</h2>
