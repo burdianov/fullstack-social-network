eefaac758c4375ee69e80ab9b0f33425d9a8e51c
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-10-17 22:40:49 +0400

    private route

---
+++ react-front/src/api/PrivateRoute.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Redirect, Route} from 'react-router-dom';
+import {isAuthenticated} from "../auth";
+
+const PrivateRoute = ({Component, ...rest}) => {
+  return (
+    <Route {...rest} render={props => isAuthenticated() ? (
+      <Component {...props}/>
+    ) : (
+      <Redirect to={{pathname: "/signin", state: {from: props.location}}}/>
+    )}/>
+  )
+};
+
+export default PrivateRoute;
