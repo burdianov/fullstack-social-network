9db7bdfb40a04259a6bedbc7a0924b72c3754c04
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-10-23 22:58:42 +0400

    node api - who to follow

--- nodeapi/routes/user.js
+++ nodeapi/routes/user.js
@@ -9,7 +9,8 @@ const {
   addFollowing,
   addFollower,
   removeFollowing,
-  removeFollower
+  removeFollower,
+  findPeople
 } = require("../controllers/user");
 const {requireSignIn} = require("../controllers/auth");
 
@@ -23,6 +24,9 @@ router.get("/user/:userId", requireSignIn, getUser);
 router.put("/user/:userId", requireSignIn, updateUser);
 router.delete("/user/:userId", requireSignIn, deleteUser);
 router.get("/user/photo/:userId", userPhoto);
+
+// who to follow
+router.get("/user/findpeople/:userId", requireSignIn, findPeople);
 
 // for any route containing :userId, the app will first execute userById()
 router.param("userId", userById);
