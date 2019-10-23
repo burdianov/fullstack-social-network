400483d1c4522328724a60837cf42adebad5c71f
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-10-23 22:48:31 +0400

    display following list

--- react-front/src/components/user/ProfileTabs.jsx
+++ react-front/src/components/user/ProfileTabs.jsx
@@ -11,29 +11,54 @@ const ProfileTabs = (props) => {
         <div className="col-md-4">
           <h3 className="text-primary">Followers</h3>
           <hr/>
-          {followers.map((follower, i) => (
+          {followers.map((person, i) => (
               <div key={i}>
-                <div className="row">
-                  <div>
-                    <Link to={`/user/${follower._id}`}>
-                      <img
-                        className="float-left mr-2"
-                        height="30px"
-                        onError={image => (image.target.src = `${defaultAvatar}`)}
-                        src={`${process.env.REACT_APP_API_URL}/user/photo/${follower._id}`}
-                        alt={follower.name}/>
-                      <div>
-                        <h3>{follower.name}</h3>
-                      </div>
-                    </Link>
-                    <p style={{clear: "both"}}>
-                      {follower.about}
-                    </p>
-                  </div>
+                <div>
+                  <Link to={`/user/${person._id}`}>
+                    <img
+                      style={{borderRadius: "50%", border: "1px solid grey"}}
+                      className="float-left mr-2"
+                      height="30px"
+                      width="30px"
+                      onError={image => (image.target.src = `${defaultAvatar}`)}
+                      src={`${process.env.REACT_APP_API_URL}/user/photo/${person._id}`}
+                      alt={person.name}/>
+                    <div>
+                      <p className="lead">{person.name}</p>
+                    </div>
+                  </Link>
                 </div>
               </div>
             )
           )}
+        </div>
+        <div className="col-md-4">
+          <h3 className="text-primary">Following</h3>
+          <hr/>
+          {following.map((person, i) => (
+              <div key={i}>
+                <div>
+                  <Link to={`/user/${person._id}`}>
+                    <img
+                      style={{borderRadius: "50%", border: "1px solid grey"}}
+                      className="float-left mr-2"
+                      height="30px"
+                      width="30px"
+                      onError={image => (image.target.src = `${defaultAvatar}`)}
+                      src={`${process.env.REACT_APP_API_URL}/user/photo/${person._id}`}
+                      alt={person.name}/>
+                    <div>
+                      <p className="lead">{person.name}</p>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            )
+          )}
+        </div>
+        <div className="com-md-4">
+          <h3 className="text-primary">Posts</h3>
+          <hr/>
         </div>
       </div>
     </div>
