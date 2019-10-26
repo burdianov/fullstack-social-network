f2a2b7693bb78c5fb2fedee9b67e352017e00cdc
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-10-26 16:47:25 +0400

    display single post

--- react-front/src/components/post/SinglePost.jsx
+++ react-front/src/components/post/SinglePost.jsx
@@ -1,5 +1,7 @@
 import React, {useEffect, useState} from "react";
 import {getSinglePost} from "../../api/post";
+import defaultPostPhoto from "../../assets/images/mountains.jpg";
+import {Link} from "react-router-dom";
 
 const SinglePost = (props) => {
   const [post, setPost] = useState("");
@@ -15,12 +17,39 @@ const SinglePost = (props) => {
       });
   }, []);
 
+  const renderPost = () => {
+    const posterId = post.postedBy ? `/user/${post.postedBy._id}` : "";
+    const posterName = post.postedBy ? post.postedBy.name : " Unknown";
+    return (
+      <div className="card-body">
+        <img
+          src={`${process.env.REACT_APP_API_URL}/post/photo/${post._id}`}
+          alt={post.title}
+          onError={image => image.target.src = `${defaultPostPhoto}`}
+          className="img-thumbnail mb-3"
+          style={{height: "300px", width: "100%", objectFit: "cover"}}/>
+        <p className="card-text">{post.body}</p>
+        <br/>
+        <p className="font-italic mark">
+          Posted by <Link to={`${posterId}`}>{posterName}</Link>
+          {" "} on {new Date(post.createdAt).toDateString()}
+        </p>
+        <Link
+          to={"/"}
+          className="btn btn-primary btn-raised btn-sm"
+        >
+          Back to posts
+        </Link>
+      </div>
+    );
+  };
+
   return (
-    <div>
-      <h2>Single post</h2>
-      {JSON.stringify(post)}
+    <div className="container">
+      <h2 className="display-2 mt-5 mb-5">{post.title}</h2>
+      {renderPost(post)}
     </div>
-  )
+  );
 };
 
 export default SinglePost;
